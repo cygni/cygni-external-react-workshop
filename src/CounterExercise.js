6faef47bbf87c5e0f3ea5b7d39bd6e6a6eeea426
Malin Hanak <malinehanak@gmail.com>
2020-01-20 09:45:08 +0100

    counter

--- src/CounterExercise.js
+++ src/CounterExercise.js
@@ -1,6 +1,13 @@
-import React, { useState } from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
 
 export default function ClickCounterExercise() {
-  return <p>CounterExercise</p>;
+  const [count, setCount] = useState(0);
+  return (
+    <section>
+      <h1>CounterExercise</h1>
+      <h2>{count}</h2>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </section>
+  );
 }
