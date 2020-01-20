c7bb1e90273182a4e88b9138b92591a110eb83e3
Malin Hanak <malinehanak@gmail.com>
2020-01-20 12:26:20 +0100

    styles

--- src/styles.js
+++ src/styles.js
@@ -1,11 +1,11 @@
-import styled, { keyframes } from "styled-components";
-import React from "react";
+import styled, { keyframes } from 'styled-components';
+import React from 'react';
 
 export const MyApp = styled.div`
   * {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-      "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-      "Helvetica Neue", sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto',
+      'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans',
+      'Helvetica Neue', sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
   }
@@ -14,7 +14,7 @@ export const MyApp = styled.div`
 `;
 
 export const Code = styled.code`
-  font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
+  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
     monospace;
 `;
 
@@ -46,4 +46,28 @@ export const AppLogo = styled.img`
   animation: ${rotate360} infinite 20s linear;
   height: 40vmin;
   pointer-events: none;
+`;
+
+export const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+`;
+
+export const Counter = styled.h2`
+  font-size: 8em;
+  flex-grow: 1;
+  text-align: center;
+  flex: 100%;
+  color: ${(props) => props.color};
+`;
+
+export const Button = styled.button`
+  border-radius: 3px;
+  background-color: white;
+  margin: 1em;
+  padding: 0.25em 1em;
+  min-width: 25px;
+  max-width: 50px;
+  flex: auto;
 `;
