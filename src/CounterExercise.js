6aa059a66ddaa2ee073a4bf2f9417a5f657a4098
Malin Hanak <malinehanak@gmail.com>
2020-01-20 09:48:38 +0100

    CounterExercise complete without style

--- src/CounterExercise.js
+++ src/CounterExercise.js
@@ -5,9 +5,10 @@ export default function ClickCounterExercise() {
   const [count, setCount] = useState(0);
   return (
     <section>
-      <h1>CounterExercise</h1>
       <h2>{count}</h2>
       <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </section>
   );
 }
